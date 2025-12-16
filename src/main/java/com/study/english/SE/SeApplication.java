6c689d9ca7943c7293fbd6d5691dde58a1bc1fdd
Nguyen Van Vu <vunv@itsol.com>
2025-12-16 16:36:02 +0700

    setup project

---
+++ src/main/java/com/study/english/SE/SeApplication.java
@@ -0,0 +1,12 @@
+package com.study.english.SE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SeApplication.class, args);
+	}
+}
